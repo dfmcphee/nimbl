30b397fb8e846f4734dd307aadf41a67d30bdf6d
Dominic McPhee <dominic.mcphee@gmail.com>
2012-07-17 23:54:07 -0400

    Making sp, bvp, and hour estimate accessible.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ActiveRecord::Base
   belongs_to :stage
   belongs_to :user
   belongs_to :sprint
-  attr_accessible :description, :name, :stage_id, :user_id, :sprint_id
+  attr_accessible :description, :name, :stage_id, :user_id, :sprint_id, :hour_estimate, :sp, :bvp
 end
